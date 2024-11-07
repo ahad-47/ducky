22c1346343c194360029d91eae8e5a38cebd2015
ahad-47 <ahad0070@hotmail.com>
2024-11-07 05:55:47 +0530

    script.ps1

--- script.ps1
+++ script.ps1
@@ -1,12 +1,8 @@
-#Enable Network Discovery
 netsh advfirewall firewall set rule group=”network discovery” new enable=yes
 
-#Enable File and Print
 netsh firewall set service type=fileandprint mode=enable profile=all
 
-#Setting Registry Values for allowing access to drive without credentials
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name everyoneincludesanonymous -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters\" -Name restrictnullsessacces -Value 0 -Force
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name anonymous -Value 1 -Force
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters\" -Name anon -Value 0 -Force
 
-#Sharing the Drive
-New-SmbShare -Name "Windows Update" -Path "C:\"
+New-SmbShare -Name "Test" -Path "C:\"
